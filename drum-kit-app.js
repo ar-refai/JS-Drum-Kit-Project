30b234c0716165fef4c122dd49612e0c26e56091
Abdelrahman Elrefai <team.man00@gmail.com>
2024-01-08 19:44:20 +0200

    js drum kit project

---
+++ drum-kit-app.js
@@ -0,0 +1,21 @@
+window.addEventListener('keydown', function(e) {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+
+    const keys = document.querySelectorAll('.key');
+    
+    function removeTransition(e) {
+        this.classList.remove('playing');
+    }
+    // remove transition by calling removeTransition() function when click
+    keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+
+});
